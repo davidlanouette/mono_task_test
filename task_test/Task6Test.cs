179e078b28950143ca5bd810463c6b95ba656c5b
David Lanouette <david_lanouette@dell.com>
2013-05-08 15:35:31 -0400

    test change

--- task_test/Task6Test.cs
+++ task_test/Task6Test.cs
@@ -1,11 +1,33 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
+
 
 namespace task_test
 {
-	public class Task6Test
+	public class Task6Test : BaseTask
 	{
-		public Task6Test ()
+		public void Run()
 		{
+			bool listening = true;
+			long i = 0;
+			while (listening)
+			{
+				i++;
+
+				Func<int> fun = () => {
+					int n = GetRandomInt(100);
+					Fib f = new Fib();
+					FibArgs args = new FibArgs();
+					args.n = n;
+
+					return f.Calculate(args);
+				};
+
+				var t = Task<int>.Factory.FromAsync(fun.BeginInvoke, fun.EndInvoke, null);
+				t = null;
+
+			}
 		}
 	}
 }
